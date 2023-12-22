8337cfa400377a89f204cd5e891792f1a1346e52
Your Name <you@example.com>
2023-12-22 19:58:02 +0530

    FinishScore & restart quiz

--- src/components/NextButtoon.js
+++ src/components/NextButtoon.js
@@ -1,15 +1,26 @@
 /** @format */
 
-function NextButtoon({ dispatch, answer }) {
+function NextButtoon({ dispatch, answer, index, numQuestions }) {
   if (answer === null) return null;
-  return (
-    <button
-      className="btn btn-ui"
-      onClick={() => dispatch({ type: "nextQuestion" })}
-    >
-      Next
-    </button>
-  );
+  if (index < numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "nextQuestion" })}
+      >
+        Next
+      </button>
+    );
+
+  if (index === numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
 }
 
 export default NextButtoon;
